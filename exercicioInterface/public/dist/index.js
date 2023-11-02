6c3edf0ff47477e737b43130603f25bde86e8b43
lucas <lucaspio.galvao@gmail.com>
2023-11-02 13:59:48 -0300

    praticando ts e requisições com o fetch

--- exercicioInterface/public/dist/index.js
+++ exercicioInterface/public/dist/index.js
@@ -1,13 +1,65 @@
-const url = "https://api.github.com/users/LukasPio";
-let responseData; // Declare a variável para armazenar os dados da resposta
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-    responseData = data; // Atribui o valor da resposta à variável responseData
-})
-    .catch((error) => {
-    console.error("Erro: " + error);
-});
-setTimeout(() => {
-    console.log(responseData);
-}, 100);
+const usersList = [];
+function getUserByLogin(login) {
+    const url = `https://api.github.com/users/${login}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        if (data.message == "Not Found") {
+            throw new Error("An error occured: This userName don't exist");
+        }
+        const user = {
+            name: data.name,
+            id: data.id,
+            login: data.login,
+            bio: data.bio,
+            public_repos: data.public_repos,
+            repos_url: data.repos_url,
+        };
+        usersList.push(user);
+    });
+}
+function getRepository(login) {
+    const url = `https://api.gitHub.com/users/${login}/repos`;
+    fetch(url).then((response) => response.json().then((data) => {
+        const reposList = [];
+        for (let i = 0; i < 5; i++) {
+            const oneRepository = {
+                name: data.name,
+                description: data.description,
+                fork: data.fork,
+                stargazers_count: data.stargazers_count,
+            };
+            reposList.push(oneRepository);
+        }
+        reposList.forEach((repos) => console.log(repos));
+    }));
+}
+function viewOneUser(login) {
+    const user = usersList.find((user) => user.login == login);
+    console.log(`Name: ${user.name}`);
+    console.log(`Bio: ${user.bio}`);
+    getRepository(login);
+}
+function viewAllUsers() {
+    usersList.forEach((user) => console.log(user));
+}
+function sumAllRepos() {
+    let allRepos = 0;
+    usersList.forEach((user) => {
+        allRepos += user.public_repos;
+    });
+    console.log(`Total of repository: ${allRepos}`);
+}
+function biggestCommitters() {
+    const commiters = [...usersList].sort((oneuser, otherUser) => otherUser.public_repos - oneuser.public_repos);
+    console.log("Top commiters:");
+    console.log(`1- ${commiters[0].name}, number of commits: ${commiters[0].public_repos}`);
+    console.log(`2- ${commiters[1].name}, number of commits: ${commiters[1].public_repos}`);
+    console.log(`3- ${commiters[2].name}, number of commits: ${commiters[2].public_repos}`);
+}
+getUserByLogin("LukasPio");
+getUserByLogin("guizinelemsm");
+getUserByLogin("rafaballerini");
+getUserByLogin("filipedeschamps");
+getUserByLogin("Fernanda-Kipper");
+setTimeout(() => biggestCommitters(), 800);
--- exercicioInterface/src/index.ts
+++ exercicioInterface/src/index.ts
@@ -1,16 +1,99 @@
-const url = "https://api.github.com/users/LukasPio";
-let responseData: any; // Declare a variável para armazenar os dados da resposta
+const usersList: User[] = [];
 
-fetch(url)
+interface User {
+  name: string;
+  id: number;
+  login: string;
+  bio: string;
+  public_repos: number;
+  repos_url: string;
+}
+interface Repository {
+  name: string;
+  description: string;
+  fork: boolean;
+  stargazers_count: number;
+}
+function getUserByLogin(login: string) {
+  const url: string = `https://api.github.com/users/${login}`;
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        responseData = data; // Atribui o valor da resposta à variável responseData
-
-    })
-    .catch((error) => {
-        console.error("Erro: " + error);
+      if (data.message == "Not Found") {
+        throw new Error("An error occured: This userName don't exist");
+      }
+      const user: User = {
+        name: data.name,
+        id: data.id,
+        login: data.login,
+        bio: data.bio,
+        public_repos: data.public_repos,
+        repos_url: data.repos_url,
+      };
+      usersList.push(user);
     });
+}
 
-setTimeout(() => {
-    console.log(responseData)
-}, 100)
+function getRepository(login: string) {
+  const url: string = `https://api.gitHub.com/users/${login}/repos`;
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      const reposList: Repository[] = [];
+
+      for (let i = 0; i < 5; i++) {
+        const oneRepository = {
+          name: data.name,
+          description: data.description,
+          fork: data.fork,
+          stargazers_count: data.stargazers_count,
+        };
+        reposList.push(oneRepository);
+      }
+
+      reposList.forEach((repos) => console.log(repos));
+    }),
+  );
+}
+
+function viewOneUser(login: string) {
+  const user = usersList.find((user) => user.login == login);
+  console.log(`Name: ${user.name}`);
+  console.log(`Bio: ${user.bio}`);
+  getRepository(login);
+}
+
+function viewAllUsers() {
+  usersList.forEach((user) => console.log(user));
+}
+
+function sumAllRepos() {
+  let allRepos: number = 0;
+  usersList.forEach((user) => {
+    allRepos += user.public_repos;
+  });
+  console.log(`Total of repository: ${allRepos}`);
+}
+
+function biggestCommitters() {
+  const commiters = [...usersList].sort(
+    (oneuser, otherUser) => otherUser.public_repos - oneuser.public_repos,
+  );
+  console.log("Top commiters:")
+  console.log(
+    `1- ${commiters[0].name}, number of commits: ${commiters[0].public_repos}`,
+  );
+  console.log(
+    `2- ${commiters[1].name}, number of commits: ${commiters[1].public_repos}`,
+  );
+  console.log(
+    `3- ${commiters[2].name}, number of commits: ${commiters[2].public_repos}`,
+  );
+}
+
+getUserByLogin("LukasPio");
+getUserByLogin("guizinelemsm");
+getUserByLogin("rafaballerini");
+getUserByLogin("filipedeschamps");
+getUserByLogin("Fernanda-Kipper");
+setTimeout(() => biggestCommitters(), 800);
